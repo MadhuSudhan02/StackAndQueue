8a9682d45d519450212e9492afd1c2394dc5e992
Madhu Sudhan <m02sudhan@gmail.com>
2022-10-13 17:53:00 +0530

    Linkedlist Stack

---
+++ LinkedListStack.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+public class LinkedListStack {
+    public static void main(String[] args) {
+        Stack<String> animal = new Stack<>();
+        //push method
+        animal.push("Lion") ;
+        animal.push("cat");
+        animal.push("Dog");
+        animal.push("Pig");
+        System.out.println(animal);
+//peek method
+        System.out.println(animal.peek());
+        System.out.println(animal);
+        //pop method
+        System.out.println(animal.pop());
+        System.out.println(animal);
+
+    }
+}
